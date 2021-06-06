134d32cb9f19a6c3dd228364becd1ff1afe8a2f4
Patrick Svensson <ps.digitalepost@gmail.com>
2021-06-06 21:59:04 +0200

    Add carousel function to testamonial card.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-/* ***** Scroll Down Icon ***** */
+/* ****** Scroll Down Icon ****** */
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
@@ -7,3 +7,28 @@ window.addEventListener("scroll", () => {
         document.querySelector(".c-scroll-icon").classList.remove("scrollIcon");
     }
 });
+
+/* ****** Testamonial Carousel ****** */
+
+let cardIndex = 0;
+
+function showTestCards() {
+    let i;
+    const cards = document.getElementsByClassName("testamonialCards");
+    const dots = document.getElementsByClassName("c-test-carousel__dot");
+    for (i = 0; i < cards.length; i++) {
+        cards[i].style.display = "none";
+    }
+    cardIndex++;
+    if (cardIndex > cards.length) {
+        cardIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activeDot", "");
+    }
+    cards[cardIndex - 1].style.display = "flex";
+    dots[cardIndex - 1].className += " activeDot";
+    setTimeout(showTestCards, 5000);
+}
+
+showTestCards();
