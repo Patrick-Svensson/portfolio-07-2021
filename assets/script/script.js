884e2bcbfb520b6a68f80edf043bb499fd7af02b
Patrick Svensson <ps.digitalepost@gmail.com>
2021-07-02 20:48:00 +0200

    Refactor Project Modal Toggle, add forEach function, remove switch.

--- assets/script/script.js
+++ assets/script/script.js
@@ -74,92 +74,18 @@ fetch("assets/script/projects.json")
 // ***** Toggle Projects Modal *****
 
 function projectImgId(clicked) {
-    switch (true) {
-        case clicked == projects[0].projectNumber:
+    projects.forEach(function (project) {
+        if (clicked == project.projectNumber) {
             modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "the portfolio";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/portfolio.png";
-            projectslink2.innerText = "...view source code";
-            projectslink2.href = "https://github.com/Patrick-Svensson/portfolio-05-2021";
-            break;
-        case clicked == projects[1].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "coloourix";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/colourix.png";
-            projectslink1.innerText = "...view source code";
-            projectslink1.href = "https://github.com/Patrick-Svensson/colourix";
-            projectslink2.innerText = "...view website";
-            projectslink2.href = "#";
-            break;
-        case clicked == projects[2].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "sean connery quiz";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/sean-connery-quiz.png";
-            projectslink1.innerText = "...view source code";
-            projectslink1.href = "https://github.com/Patrick-Svensson/sean-connery-quiz";
-            projectslink2.innerText = "...view website";
-            projectslink2.href = "https://patrick-svensson.github.io/sean-connery-quiz/";
-            break;
-        case clicked == projects[3].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "gaffer's dock";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/gaffers-dock.png";
-            projectslink1.innerText = "...view source code";
-            projectslink1.href = "https://github.com/Patrick-Svensson/gaffers-dock";
-            projectslink2.innerText = "...view website";
-            projectslink2.href = "https://patrick-svensson.github.io/gaffers-dock/";
-            break;
-        case clicked == projects[4].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "bhromaon";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/bhromaon.png";
-            projectslink1.innerText = "...view source code";
-            projectslink1.href = "https://github.com/Patrick-Svensson/bhromaon";
-            projectslink2.innerText = "...view website";
-            projectslink2.href = "https://patrick-svensson.github.io/bhromaon/";
-            break;
-        case clicked == projects[5].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/emoji-mischief.png";
-            projectslink1.innerText = "...view source code";
-            projectslink1.href = "https://github.com/Patrick-Svensson/svg-in-play";
-            projectslink2.innerText = "...view website";
-            projectslink2.href = "#";
-            break;
-        case clicked == projects[6].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "time paradox";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/time-paradox.png";
-            projectslink2.innerText = "...view image";
-            projectslink2.href = "../../assets/images/time-paradox.png";
-            break;
-        case clicked == projects[7].projectNumber:
-            modal.classList.toggle("activateModalContainer");
-            projectsHeading.innerText = "movie theatre logo";
-            projectsDescription.innerText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            projectsImage.src = "../../assets/images/movie-theatre.png";
-            projectslink2.innerText = "...view design";
-            projectslink2.href = "../../assets/images/movie-theatre.png";
-            break;
-        default:
-            return;
-    }
+            projectsHeading.innerText = ` ${project.heading}`;
+            projectsDescription.innerText = ` ${project.description}`;
+            projectsImage.src = ` ${project.image}`;
+            projectslink1.innerText = ` ${project.linkText1}`;
+            projectslink1.href = ` ${project.link1}`;
+            projectslink2.innerText = ` ${project.linkText2}`;
+            projectslink2.href = ` ${project.link2}`;
+        }
+    });
 }
 
 closeButton.addEventListener("click", () => {
